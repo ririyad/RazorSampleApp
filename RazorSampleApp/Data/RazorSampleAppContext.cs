fda504e3d3c4996363c66bfdb2bf29a0febf070c
ririyad <rasheduliriyad@gmail.com>
2020-11-11 17:47:24 +0600

    Inital commit, added sample app

---
+++ RazorSampleApp/Data/RazorSampleAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorSampleApp.Models;
+
+namespace RazorSampleApp.Data
+{
+    public class RazorSampleAppContext : DbContext
+    {
+        public RazorSampleAppContext (DbContextOptions<RazorSampleAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorSampleApp.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ RazorSampleApp/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorSampleApp.Data;
+using RazorSampleApp.Models;
+
+namespace RazorSampleApp.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorSampleApp.Data.RazorSampleAppContext _context;
+
+        public IndexModel(RazorSampleApp.Data.RazorSampleAppContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
